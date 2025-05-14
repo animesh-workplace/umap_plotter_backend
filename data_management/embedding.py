08e05aa7861fa9388c5751d974086db2e4bfdc64
Animesh <animesh.workplace@gmail.com>
2025-05-14 13:20:49 +0530

    updated file - data_management/embedding.py

--- data_management/embedding.py
+++ data_management/embedding.py
@@ -1,5 +1,14 @@
-from database import UMAPEmbeddingTable
+from sqlalchemy import func
+from database import UMAP2DEmbeddingTable, UMAP3DEmbeddingTable
 
-def get_umap_embeddings(db):
-    embeddings = db.query(UMAPEmbeddingTable).order_by(UMAPEmbeddingTable.cell_id).all()
+def get_umap_embeddings_2d(db):
+    embeddings = db.query(UMAP2DEmbeddingTable).order_by(UMAP2DEmbeddingTable.cell_id).all()
+    return embeddings
+
+def get_unique_cell_type_2d(db):
+    cell_name = db.query(func.split_part(UMAP2DEmbeddingTable.cell_id, '_', 1)).distinct().all()
+    return [expr[0] for expr in cell_name]
+
+def get_umap_embeddings_3d(db):
+    embeddings = db.query(UMAP3DEmbeddingTable).order_by(UMAP3DEmbeddingTable.cell_id).all()
     return embeddings
