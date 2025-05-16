e20f61697e013ea9ea33c90fd65a6db8c3d0ab3c
Animesh <animesh.workplace@gmail.com>
2025-05-17 00:18:20 +0530

    updated file - data_management/embedding.py

--- data_management/embedding.py
+++ data_management/embedding.py
@@ -1,14 +1,42 @@
 from sqlalchemy import func
-from database import UMAP2DEmbeddingTable, UMAP3DEmbeddingTable
+from database import (
+    UMAP2DEmbeddingTable,
+    UMAP3DEmbeddingTable,
+    TSNE3DEmbeddingTable,
+    TSNE2DEmbeddingTable,
+)
 
-def get_umap_embeddings_2d(db):
-    embeddings = db.query(UMAP2DEmbeddingTable).order_by(UMAP2DEmbeddingTable.cell_id).all()
-    return embeddings
 
 def get_unique_cell_type_2d(db):
-    cell_name = db.query(func.split_part(UMAP2DEmbeddingTable.cell_id, '_', 1)).distinct().all()
+    cell_name = (
+        db.query(func.split_part(UMAP2DEmbeddingTable.cell_id, "_", 1)).distinct().all()
+    )
     return [expr[0] for expr in cell_name]
 
+
+def get_umap_embeddings_2d(db):
+    embeddings = (
+        db.query(UMAP2DEmbeddingTable).order_by(UMAP2DEmbeddingTable.cell_id).all()
+    )
+    return embeddings
+
+
 def get_umap_embeddings_3d(db):
-    embeddings = db.query(UMAP3DEmbeddingTable).order_by(UMAP3DEmbeddingTable.cell_id).all()
+    embeddings = (
+        db.query(UMAP3DEmbeddingTable).order_by(UMAP3DEmbeddingTable.cell_id).all()
+    )
+    return embeddings
+
+
+def get_tsne_embeddings_3d(db):
+    embeddings = (
+        db.query(TSNE3DEmbeddingTable).order_by(TSNE3DEmbeddingTable.cell_id).all()
+    )
+    return embeddings
+
+
+def get_tsne_embeddings_2d(db):
+    embeddings = (
+        db.query(TSNE2DEmbeddingTable).order_by(TSNE2DEmbeddingTable.cell_id).all()
+    )
     return embeddings
