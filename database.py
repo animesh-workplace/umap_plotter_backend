e25a6de86e4a1e3ad6485392d25e2457866d7542
Animesh <animesh.workplace@gmail.com>
2025-05-17 00:18:20 +0530

    updated file - database.py

--- database.py
+++ database.py
@@ -5,11 +5,14 @@ from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 
 # Load your DB credentials from environment variables or hardcode for local dev
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://animesh:@localhost:5432/GeneEchelon")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", "postgresql://animesh:@localhost:5432/GeneEchelon"
+)
 
 # Create the engine and local session
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 # Dependency to get DB session in FastAPI routes
 def get_db():
@@ -19,8 +22,10 @@ def get_db():
     finally:
         db.close()
 
+
 # Base class for declarative models
 Base = declarative_base()
+
 
 class UMAP2DEmbeddingTable(Base):
     __tablename__ = "data_management_2dumapembedding"
@@ -29,6 +34,7 @@ class UMAP2DEmbeddingTable(Base):
     cluster = Column(String)
     cell_id = Column(String, index=True)
     id = Column(Integer, primary_key=True, index=True)
+
 
 class UMAP3DEmbeddingTable(Base):
     __tablename__ = "data_management_3dumapembedding"
@@ -39,10 +45,31 @@ class UMAP3DEmbeddingTable(Base):
     cell_id = Column(String, index=True)
     id = Column(Integer, primary_key=True, index=True)
 
+
+class TSNE2DEmbeddingTable(Base):
+    __tablename__ = "data_management_2dtsneembedding"
+    x = Column(Float)
+    y = Column(Float)
+    cluster = Column(String)
+    cell_id = Column(String, index=True)
+    id = Column(Integer, primary_key=True, index=True)
+
+
+class TSNE3DEmbeddingTable(Base):
+    __tablename__ = "data_management_3dtsneembedding"
+    x = Column(Float)
+    y = Column(Float)
+    z = Column(Float)
+    cluster = Column(String)
+    cell_id = Column(String, index=True)
+    id = Column(Integer, primary_key=True, index=True)
+
+
 class GeneTable(Base):
     __tablename__ = "data_management_gene"
     name = Column(String, index=True)
     id = Column(Integer, primary_key=True, index=True)
+
 
 class GeneExpressionTable(Base):
     __tablename__ = "data_management_geneexpression"
