1b32b7e871c3343efa22a246a6dd60fdfec33bc7
Animesh <animesh.workplace@gmail.com>
2025-06-20 10:36:57 +0530

    updated file - schema.py

--- schema.py
+++ schema.py
@@ -14,3 +14,25 @@ class UMAP3DEmbeddingSchema(BaseModel):
     z: float
     cell_id: str
     cluster: str
+
+
+class SpatialPositionSchema(BaseModel):
+    x: int
+    y: int
+    sample: str
+    cell_id: str
+    background: int
+
+
+class SpatialExpressionSchema(BaseModel):
+    sample: str
+    cell_id: str
+    caf_1: float
+    caf_2: float
+    caf_3: float
+    caf_4: float
+    caf_5: float
+    caf_6: float
+    caf_7: float
+    caf_8: float
+    caf_9: float
